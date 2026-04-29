8d316fecf510925622c73d755551084b30a894b7
Lfeliquis <luisg.felix.correia@gmail.com>
2026-04-28 22:47:35 -0300

    alterando a rota do frontend

--- frontend/src/pages/Pets.jsx
+++ frontend/src/pages/Pets.jsx
@@ -15,7 +15,7 @@ function Pets() {
   useEffect(() => {
     async function fetchPets() {
       try {
-        const response = await api.get("/api/pets");
+        const response = await api.get("/pets");
         setPets(response.data);
       } catch (err) {
         setError("Erro ao carregar os pets");
