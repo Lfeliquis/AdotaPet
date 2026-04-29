eec6ab3773c348fa8f018a5c0a2228060621c0f0
Lfeliquis <luisg.felix.correia@gmail.com>
2026-04-28 21:32:51 -0300

    alteracoes no server

--- backend/server.js
+++ backend/server.js
@@ -3,29 +3,30 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
+
 const authRoutes = require("./src/routes/auth.routes");
 const userRoutes = require("./src/routes/user.routes");
 const petRoutes = require("./src/routes/pet.routes");
 const adoptionRoutes = require("./src/routes/adoption.routes");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+// ROTAS ORGANIZADAS
 app.use("/api/auth", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", petRoutes);
-app.use("/api", adoptionRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/pets", petRoutes);
+app.use("/api/adoptions", adoptionRoutes);
 
 // Conexão com banco
 connectDB();
 
 // Rota teste
 app.get("/", (req, res) => {
-  res.send("API rodando ");
+  res.send("API rodando 🚀");
 });
 
 // Subir servidor
