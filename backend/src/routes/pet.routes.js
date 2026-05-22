fcd1b56fa170df14d1145f9efb15a08d17b7bede
Lfeliquis <luisg.felix.correia@gmail.com>
2026-05-21 22:47:24 -0300

    fix pet routes order

--- backend/src/routes/pet.routes.js
+++ backend/src/routes/pet.routes.js
@@ -1,33 +1,25 @@
-const express = require("express");
-const router = express.Router();
-const PetController = require("../controllers/PetController");
-const authMiddleware = require("../middlewares/authMiddleware");
-const upload = require("../middlewares/upload");
-
 router.post(
-  "/",
+  "/pets",
   authMiddleware,
   upload.single("image"),
   PetController.createPet,
 );
 
-router.get("/", PetController.getPets);
+router.get("/pets", PetController.getPets);
 
 router.get("/my-pets", authMiddleware, PetController.getMyPets);
 
 router.get("/my-adoptions", authMiddleware, PetController.getMyAdoptions);
 
-router.get("/:id", PetController.getPetById);
+router.get("/pets/:id", PetController.getPetById);
 
 router.put(
-  "/:id",
+  "/pets/:id",
   authMiddleware,
   upload.single("image"),
   PetController.updatePet,
 );
 
-router.delete("/:id", authMiddleware, PetController.deletePet);
+router.delete("/pets/:id", authMiddleware, PetController.deletePet);
 
-router.post("/:id/adopt", authMiddleware, PetController.adoptPet);
-
-module.exports = router;
+router.post("/pets/:id/adopt", authMiddleware, PetController.adoptPet);
