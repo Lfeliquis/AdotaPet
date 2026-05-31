5b37994d9aaced75969cc3d9cd130385c7c95705
Lfeliquis <luisg.felix.correia@gmail.com>
2026-05-31 14:12:06 -0300

    alteracoes user role

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,13 +1,17 @@
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ children }) {
+export default function ProtectedRoute({ children, adminOnly = false }) {
   const token = localStorage.getItem("token");
 
+  const user = JSON.parse(localStorage.getItem("user"));
+
   if (!token) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" />;
+  }
+
+  if (adminOnly && user?.role !== "admin") {
+    return <Navigate to="/" />;
   }
 
   return children;
 }
-
-export default ProtectedRoute;
